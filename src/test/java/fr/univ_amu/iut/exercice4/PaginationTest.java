6ea9c6d56ad0acace0f28f0007d48875fc2cf971
CHARDONNET Kyle <ch.kyle07@gmail.com>
2026-05-21 15:29:25 +0000

    Exercice 4 terminé

--- src/test/java/fr/univ_amu/iut/exercice4/PaginationTest.java
+++ src/test/java/fr/univ_amu/iut/exercice4/PaginationTest.java
@@ -2,7 +2,6 @@ package fr.univ_amu.iut.exercice4;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -24,62 +23,51 @@ import org.junit.jupiter.api.Test;
  */
 class PaginationTest {
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_d_une_seule_page_n_affiche_qu_elle_meme() {
     assertThat(new Pagination(1, 1).afficher()).isEqualTo("(1)");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_5_pages_avec_courant_au_milieu_affiche_toutes_les_pages() {
     assertThat(new Pagination(3, 5).afficher()).isEqualTo("1 2 (3) 4 5");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_5_pages_avec_courant_au_debut_affiche_toutes_les_pages() {
     assertThat(new Pagination(1, 5).afficher()).isEqualTo("(1) 2 3 4 5");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_5_pages_avec_courant_a_la_fin_affiche_toutes_les_pages() {
     assertThat(new Pagination(5, 5).afficher()).isEqualTo("1 2 3 4 (5)");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_7_pages_affiche_toutes_les_pages() {
     assertThat(new Pagination(4, 7).afficher()).isEqualTo("1 2 3 (4) 5 6 7");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_15_pages_avec_courant_au_milieu_affiche_des_ellipses_des_deux_cotes() {
     assertThat(new Pagination(7, 15).afficher()).isEqualTo("1 ... 6 (7) 8 ... 15");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_15_pages_avec_courant_pres_du_debut_n_affiche_qu_une_ellipse_a_droite() {
     assertThat(new Pagination(3, 15).afficher()).isEqualTo("1 2 (3) 4 ... 15");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_15_pages_avec_courant_pres_de_la_fin_n_affiche_qu_une_ellipse_a_gauche() {
     assertThat(new Pagination(13, 15).afficher()).isEqualTo("1 ... 12 (13) 14 15");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   void la_pagination_de_15_pages_avec_courant_en_seconde_position() {
     assertThat(new Pagination(2, 15).afficher()).isEqualTo("1 (2) 3 ... 15");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
-  @Test
   void la_pagination_de_15_pages_avec_courant_en_avant_derniere_position() {
     assertThat(new Pagination(14, 15).afficher()).isEqualTo("1 ... 13 (14) 15");
   }
